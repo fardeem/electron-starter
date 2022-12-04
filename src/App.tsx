25d1fb6f50bef9f663accadea21165ac417360d9
Fardeem Munir <munirfardeem@gmail.com>
2022-12-04 17:18:22 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App(): JSX.Element {
+  return <h1 className="text-red-600">Hello World!! Wooo</h1>;
+}
---
+++ src/renderer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  throw new Error("Could not find root element.");
+}
